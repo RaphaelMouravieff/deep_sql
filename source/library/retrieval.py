c7a08b269db94cd43fc74f56a1a1f61ddebe7f8f
RaphaelMouravieff <raphael.mouravieff@gmail.com>
2025-04-17 13:53:30 +0200

    Remove unnecessary files

--- source/library/retrieval.py
+++ source/library/retrieval.py
@@ -5,26 +5,28 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 from faiss import IndexFlatIP
 
-from torch.backends import mps
+import torch
 
-def get_emebdding_model(model_name="Alibaba-NLP/gte-large-en-v1.5"):
-    device = "mps" if mps.is_available() else "cpu"
-    print(f'Device : {device}')
-    encode_kwargs={"device":device,'normalize_embeddings':True}
-    model_kwargs={"trust_remote_code":True}
-    embeddings = HuggingFaceEmbeddings(model_name=model_name,model_kwargs=model_kwargs,encode_kwargs=encode_kwargs)
+def get_emebdding_model(model_name):
+
+    device = "cpu"
+    encode_kwargs = {"device":device,'normalize_embeddings':True}
+    model_kwargs = {"trust_remote_code":True}
+    embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs)
+
     return embeddings
 
-def embeddings_vector_store(model_name="Alibaba-NLP/gte-large-en-v1.5"):
-    embeddings= get_emebdding_model(model_name)
-   
-    index = IndexFlatIP(len(embeddings.embed_query("hello world")))
+def embeddings_vector_store(model_name):
 
+    embeddings = get_emebdding_model(model_name)
+    index = IndexFlatIP(len(embeddings.embed_query("hello world")))
     vector_store = FAISS(
         embedding_function=embeddings,
         index=index,
         docstore=InMemoryDocstore(),
         index_to_docstore_id={},
-        distance_strategy=DistanceStrategy.MAX_INNER_PRODUCT,#EUCLIDEAN_DISTANCE MAX_INNER_PRODUCT
+        distance_strategy=DistanceStrategy.MAX_INNER_PRODUCT,
     )
+
     return vector_store
+
--- source/library/storage.py
+++ source/library/storage.py
@@ -3,41 +3,9 @@
 
 import os
 import json
-import torch
-
-from sentence_transformers import SentenceTransformer
-
-from faiss import IndexFlatIP
 from langchain_community.vectorstores import FAISS
-
 from source.library.retrieval import get_emebdding_model, embeddings_vector_store
 
-
-def load_sentence(name: str, hf_tokens:str ,device=None):
-    # Detect device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") if device is None else device
-
-    # Convert model name into a valid directory name
-    model_dir = "models/" + name.replace("/", "_")
-    
-    # Ensure models directory exists
-    os.makedirs("models", exist_ok=True)
-
-    # Check if model is already saved
-    if os.path.exists(model_dir):
-        print(f"Loading model from disk: {model_dir}")
-        try:
-            model = SentenceTransformer(model_dir, device=device, trust_remote_code=True)
-            return model
-        except Exception as e:
-            print(f"Error loading model from {model_dir}, redownloading...\n{e}")
-
-    # Download and save the model
-    print(f"Downloading model: {name}")
-    model = SentenceTransformer(name, device=device, trust_remote_code=True)
-    model.save(model_dir)
-    
-    return model
 
 
 def init_library(data_args, training_args):
