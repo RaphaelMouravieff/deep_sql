a85c4130eff14e82eed8161001b0d0ce808e4800
TanguySki <tanguyherserant@gmail.com>
2025-06-03 16:07:03 +0200

    add lock on library

--- source/library/storage.py
+++ source/library/storage.py
@@ -1,8 +1,6 @@
-
-
-
 import os
 import json
+import fcntl
 from langchain_community.vectorstores import FAISS
 from source.library.retrieval import get_emebdding_model, embeddings_vector_store
 
@@ -26,9 +24,29 @@ def init_library(data_args):
 def save_library(data_args,
                  library, 
                  vector_store):
-
-    with open(data_args.library_path, 'w') as f:
-        json.dump(library, f, indent=2)
-    vector_store.save_local(data_args.vector_store_path)
+    # Create a lock file path
+    lock_file_path = f"{data_args.library_path}.lock"
+    
+    # Open the lock file
+    with open(lock_file_path, 'w') as lock_file:
+        try:
+            # Acquire an exclusive lock
+            fcntl.flock(lock_file.fileno(), fcntl.LOCK_EX)
+            
+            # Write to the library file
+            with open(data_args.library_path, 'w') as f:
+                json.dump(library, f, indent=2)
+            
+            # Save vector store
+            vector_store.save_local(data_args.vector_store_path)
+            
+        finally:
+            # Release the lock
+            fcntl.flock(lock_file.fileno(), fcntl.LOCK_UN)
+            # Remove the lock file
+            try:
+                os.remove(lock_file_path)
+            except OSError:
+                pass  # Ignore if file was already removed
 
 
